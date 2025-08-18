329390bbffade63a6775b02b62883414f9d86912
Alexia-Crisan <150911612+Alexia-Crisan@users.noreply.github.com>
2025-08-18 12:29:13 +0300

    player photo with sprites added

--- main.py
+++ main.py
@@ -14,9 +14,36 @@ PLAYER_VELOCITY = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+    for image in images:
+        sprtie_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        sprites = []
+
+        for i in range(sprtie_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprtie_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -44,13 +71,14 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
 
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprites = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprites, (self.rect.x, self.rect.y))
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
