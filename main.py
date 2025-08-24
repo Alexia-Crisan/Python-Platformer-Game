76c1b69cdffcbea26991e224b8395372669752d3
Alexia-Crisan <150911612+Alexia-Crisan@users.noreply.github.com>
2025-08-24 14:05:05 +0300

    player animation

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
     SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
+    ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -75,10 +76,28 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprtie_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprtie_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite) # for pixel perfect collision
 
     def draw(self, window):
-        self.sprites = self.SPRITES["idle_" + self.direction][0]
-        window.blit(self.sprites, (self.rect.x, self.rect.y))
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
+
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
